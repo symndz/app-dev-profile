fb0177813090103ace82aa370f397ef9c1b68c4e
training <marcin.danilewicz@mobica.com>
2018-07-19 14:07:55 +0200

    auto layout for iphones 4_48

---
+++ dev-profile/dev-profile/View/RoundEdgesonImage.swift
@@ -0,0 +1,13 @@
+//
+//  RoundEdgesonImage.swift
+//  dev-profile
+//
+//  Created by training on 19.07.2018.
+//  Copyright © 2018 Developers. All rights reserved.
+//
+
+import UIKit
+
+class RoundEdgesOnImage: UIImageView {
+
+}
--- dev-profile/dev-profile/ViewController.swift
+++ dev-profile/dev-profile/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var devslopesProfileLogoImg: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        devslopesProfileLogoImg.layer.cornerRadius = 10
+        devslopesProfileLogoImg.clipsToBounds = true
     }
 
     override func didReceiveMemoryWarning() {
